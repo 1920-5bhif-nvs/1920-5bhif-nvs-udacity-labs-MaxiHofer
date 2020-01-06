d1ecbb12b449c6b83f970632698791ebec983d39
MaxiHofer <maximilian@alexxdesign.at>
2020-01-06 12:14:47 +0100

    10.12 - Add Material Attributes and Overlay - Maximilian Hofer

--- Lesson_10/andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ Lesson_10/andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
